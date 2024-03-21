460d2a5c2bc494c6e9561e13cdfd519f2d828cca
11335 <jack.lanning@vtsd.com>
2024-03-21 07:53:22 -0400

    .

--- 2.1.4/main.py
+++ 2.1.4/main.py
@@ -2,13 +2,22 @@
 #   A program creates a window on your screen using Tkinter.
 import tkinter as tk
 
+def test_my_button():
+    frame_auth.tkraise()
+    ent_password.tk.get()
+
 # main window
 root = tk.Tk()
 root.wm_geometry("500x500")
 
 frame_login = tk.Frame(root)
-frame_login.grid()
+frame_login.grid(row=0, column=0, sticky='news')
 
+frame_auth = tk.Frame(root)
+frame_auth.grid(row=0, column=0, sticky='news')
+frame_auth = tk.Label(frame_login, text='password', font='Arial')
+
+frame_login.tkraise()
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
@@ -22,8 +31,9 @@ lbl_password.pack()
 ent_password = tk.Entry(frame_login, bd=3)
 ent_password.pack(pady=5)
 
-btn_login = tk.Label(frame_login, text='Login:')
-btn_login.pack(pady=5)
+btn_login = tk.Button(frame_login, text='Login', bd=3, command=test_my_button)
+btn_login.pack()
 
 tk.Label(frame_login,text="Password:",font="Courier")
+
 root.mainloop()
